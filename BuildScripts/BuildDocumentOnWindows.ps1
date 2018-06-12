426d6f5a710058262c42a18ad5ec015f9e20c8a6
anionDev <marius.goecke@gmail.com>
2018-06-12 15:04:00 +0200

    change

--- BuildScripts/BuildDocumentOnWindows.ps1
+++ BuildScripts/BuildDocumentOnWindows.ps1
@@ -1,6 +1,6 @@
 Param(
   [string]$pdfLatexFile,#this parameter is not required if pdflatex.exe is available in the path-variable. Execute 'setx /M PATH "%PATH%;C:\my\pdfLatex\Folder"' as administrator for this purpose but be careful about the maximal length of the content of the path-variable.
-  [string]$texFile,#relative path based on the repository-root-folder
+  [string]$texFile,#relative path based on the repository-root-folder.
   [switch]$openDocumentAtEnd
 )
 $argument="$texFile -synctex=1 -interaction=nonstopmode"
