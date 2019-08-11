381d44e4abc90212d7e8e7318b447b41f151ca4f
Marius Göcke <marius.goecke@gmail.com>
2019-08-11 17:18:19 +0200

    fix

--- BuildDocument.py
+++ BuildDocument.py
@@ -4,5 +4,5 @@ import subprocess
 parser = ArgumentParser()
 parser.add_argument("-pdf", dest="pdflatexfile", help="pdfLatex file with full path", default="pdflatex")
 args = parser.parse_args()
-pdfLatexArgument="\"\input{cheatsheet.tex}\" -synctex=1 -interaction=nonstopmode -job-name cheatsheet -halt-on-error -output-directory ."
+pdfLatexArgument="\"\input{cheatsheet.tex}\" -synctex=1 -interaction=nonstopmode -job-name cheatsheet -halt-on-error -output-directory .."
 subprocess.call(args.pdflatexfile + " " + pdfLatexArgument)
